d60709c9a43ca42262de6d744f800618c45fa247
saipradeep_ravipati <saipradeep.ravipati@tarento.com>
2024-09-28 17:06:04 +0530

    Bug Fix

--- credential-generator/public-assessment-certificate-generator-utility/src/main/scala/org/sunbird/job/certpublic/functions/CertValidator.scala
+++ credential-generator/public-assessment-certificate-generator-utility/src/main/scala/org/sunbird/job/certpublic/functions/CertValidator.scala
@@ -179,8 +179,8 @@ class CertValidator() {
   
   def isNotIssued(event: Event)(config: CertificateGeneratorConfig, metrics: Metrics, cassandraUtil: CassandraUtil):Boolean = {
     val query = QueryBuilder.select( "issued_certificates").from(config.dbKeyspace, config.dbEnrollmentTable)
-      .where(QueryBuilder.eq(config.dbEmailId, event.eData.getOrElse("email", "")))
-      .and(QueryBuilder.eq(config.dbAssessmentId, event.related.getOrElse("assessmentid", "")))
+      .where(QueryBuilder.eq(config.dbEmailId, event.eData.getOrElse("userId", "")))
+      .and(QueryBuilder.eq(config.dbAssessmentId, event.related.getOrElse("assessmentId", "")))
     val row = cassandraUtil.findOne(query.toString)
     metrics.incCounter(config.enrollmentDbReadCount)
     if (null != row) {
